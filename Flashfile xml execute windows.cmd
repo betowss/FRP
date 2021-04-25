a4830d002ce21ee95887b9b0114e93eb88d462ce
Gorniaky <30408913+Gorniaky@users.noreply.github.com>
2021-04-25 13:59:18 -0300

    Update Flashfile xml execute windows.cmd

--- Flashfile xml execute windows.cmd
+++ Flashfile xml execute windows.cmd
@@ -1,8 +1,8 @@
 @echo off
 echo adb devices > commands.txt
 echo adb reboot-bootloader >> commands.txt
-echo fastboot devices >> commands.txt
-echo fastboot reboot-bootloader >> commands.txt
+echo mfastboot devices >> commands.txt
+echo mfastboot reboot-bootloader >> commands.txt
 if exist flashfile.xml (
 goto startconvert
 ) else (
@@ -17,24 +17,24 @@ findstr "\<software_version\>" flashfile.xml > software_version.txt
 for /F delims^=^"^ Tokens^=2^* %%G in (software_version.txt) DO set title=%%G
 title %title%
 echo @title %title% > flashfile.cmd
-echo fastboot getvar max-sparse-size >> flashfile.cmd
-echo fastboot oem fb_mode_set >> flashfile.cmd
+echo mfastboot getvar max-sparse-size >> flashfile.cmd
+echo mfastboot oem fb_mode_set >> flashfile.cmd
 findstr "\<erase\>" flashfile.xml | findstr /v "modem" > erase.txt
 findstr "\<flash\>" flashfile.xml > flash.txt
-for /F delims^=^"^ Tokens^=4^,6^,8^* %%G in (flash.txt) DO @echo fastboot %%H %%I %%G >> flashfile.cmd
-for /F delims^=^"^ Tokens^=2^,4^,6^* %%G in (erase.txt) DO @echo fastboot %%G %%H >> flashfile.cmd
-echo fastboot oem fb_mode_clear >> flashfile.cmd
-echo fastboot reboot >> flashfile.cmd
+for /F delims^=^"^ Tokens^=4^,6^,8^* %%G in (flash.txt) DO @echo mfastboot %%H %%I %%G >> flashfile.cmd
+for /F delims^=^"^ Tokens^=2^,4^,6^* %%G in (erase.txt) DO @echo mfastboot %%G %%H >> flashfile.cmd
+echo mfastboot oem fb_mode_clear >> flashfile.cmd
+echo mfastboot reboot >> flashfile.cmd
 echo @cmd >> flashfile.cmd
 del erase.txt
 del flash.txt
 del software_version.txt
-if exist fastboot.exe (
+if exist mfastboot.exe (
 goto startflash
 ) else (
-goto nofastbootexe
+goto nomfastbootexe
 )
-:nofastbootexe
+:nomfastbootexe
 findstr /v "@title" flashfile.cmd > "%title%.txt"
 exit
 :startflash
