592f8a02fedf1a88cfdf78ad0615a4acd13d5dfa
Gorniaky <30408913+Gorniaky@users.noreply.github.com>
2021-06-20 22:45:10 -0300

    Update Flashfile xml execute windows.cmd

--- Flashfile xml execute windows.cmd
+++ Flashfile xml execute windows.cmd
@@ -1,40 +1,28 @@
 @echo off
-echo adb devices > commands.txt
-echo adb reboot-bootloader >> commands.txt
-echo mfastboot devices >> commands.txt
-echo mfastboot reboot-bootloader >> commands.txt
-if exist flashfile.xml (
-goto startconvert
-) else (
-goto noflashfilexml
-)
+if exist flashfile.xml (goto startconvert) else (goto noflashfilexml)
 :noflashfilexml
 find "flashfile" flashfile.xml
 pause
 exit
 :startconvert
 findstr "\<software_version\>" flashfile.xml > software_version.txt
-for /F delims^=^"^ Tokens^=2^* %%G in (software_version.txt) DO set title=%%G
-title %title%
-echo @title %title% > flashfile.cmd
-echo mfastboot getvar max-sparse-size >> flashfile.cmd
-echo mfastboot oem fb_mode_set >> flashfile.cmd
-findstr "\<erase\>" flashfile.xml | findstr /v "modem" > erase.txt
-findstr "\<flash\>" flashfile.xml > flash.txt
-for /F delims^=^"^ Tokens^=4^,6^,8^* %%G in (flash.txt) DO @echo mfastboot %%H %%I %%G >> flashfile.cmd
-for /F delims^=^"^ Tokens^=2^,4^,6^* %%G in (erase.txt) DO @echo mfastboot %%G %%H >> flashfile.cmd
-echo mfastboot oem fb_mode_clear >> flashfile.cmd
-echo mfastboot reboot >> flashfile.cmd
-echo @cmd >> flashfile.cmd
-del erase.txt
-del flash.txt
+for /f delims^=^"^ tokens^=2 %%a in (software_version.txt) do set title=%%a
 del software_version.txt
+title %title%
+(echo @title %title%
+echo mfastboot getvar max-sparse-size
+echo mfastboot oem fb_mode_set) > flashfile.cmd
+findstr "\<flash\>" flashfile.xml > flash.txt
+findstr "\<erase\>" flashfile.xml | findstr /v "modem" > erase.txt
+for /F delims^=^"^ tokens^=4^,6^,8 %%a in (flash.txt) do @echo mfastboot %%b %%c %%a >> flashfile.cmd
+del flash.txt
+for /F delims^=^"^ tokens^=2^,4^,6 %%a in (erase.txt) do @echo mfastboot %%a %%b >> flashfile.cmd
+del erase.txt
+(echo mfastboot oem fb_mode_clear
+echo mfastboot reboot
+echo @cmd) >> flashfile.cmd
 findstr /v "@title" flashfile.cmd > "FLASHFILE COMMANDS %title%.txt"
-if exist mfastboot.exe (
-goto startflash
-) else (
-goto nomfastbootexe
-)
+if exist mfastboot.exe (goto startflash) else (goto nomfastbootexe)
 :nomfastbootexe
 exit
 :startflash
